7f964a13253fb356f95c05b38da85e28160f3257
Fernando Bretz <fernando.bretz@groupsoftware.com.br>
2021-12-14 14:27:40 -0300

    Barra de Pesquisa

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-import  django_heroku 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -140,5 +138,3 @@ STATICFILES_DIRS=[
 #Media
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-django_heroku.settings(locals())
--- receitas/views.py
+++ receitas/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Receita
-# Create your views here.
+ # Create your views here.
 
 def index(request):
     receitas = Receita.objects.order_by('-date_receita').filter(publicada=True)
@@ -15,3 +15,14 @@ def receita(request, receita_id):
         'receita': receita
     }
     return render(request,'receita.html',receita_a_exibir)
+
+def buscar(request):
+    lista_receitas = Receita.objects.order_by('-date_receita').filter(publicada=True)
+    if 'buscar' in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if buscar:
+            lista_receitas = lista_receitas.filter(nome_receita__icontains=nome_a_buscar)
+    dados={
+         'receitas': lista_receitas
+    }
+    return render(request,'index.html', dados)
