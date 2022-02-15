f7c6c9e02bceaceae93777c6974c45266ae9452b
Fernando Bretz <fernando.bretz@groupsoftware.com.br>
2022-02-15 10:38:00 -0300

    Adicionado Cadastro de Usuarios 👨

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import redirect, render
- 
+from django.contrib.auth.models import User
+
 
 def login(request):
     return render(request,'usuarios/login.html')
 
 def cadastro(request):
     if request.method=='POST':
-        print('Usuario criado com sucesso.')
+        nome= request.POST['nome']
+        email= request.POST['email']
+        password= request.POST['password']
+        password2= request.POST['password2']
+        if not nome.strip():
+            return redirect('cadastro')
+        if not email.strip():
+            return redirect('cadastro')   
+        if password!= password2:
+            return redirect('cadastro')   
+        if User.objects.filter(email=email).exists():
+            return redirect('cadastro')  
+        user =User.objects.create_user(username=nome, email= email, password= password)
+        user.save()
+        print('Usuário cadastrado com sucesso')
         return redirect('login')
     else:
         return render(request,'usuarios/cadastro.html')
